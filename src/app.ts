1d0312a0d77820a13d86e0c7788155f0c8c971ed
Luiz Almada <luizalmada@yahoo.com>
2025-10-13 13:06:55 -0300

    feat: create user, add controller register, add hash password, add user case Users, Dependency Inversion, Repository Pattern, Interface, Global errors and Specific Error

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,26 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
+import z, { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user
-  .create({
-    data: {
-      name: 'Luiz Almada',
-      email: 'luizalmada@yahoo.com',
-    },
-  })
-  .then(() => {
-    console.log('User created')
-  })
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: z.treeifyError(error),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // Registrar o log do erro em alguma ferramenta de monitoramento e observabilidade
+    // TODO: Here we should log to an external tool like DataDog, NewRelic, Sentry, etc...
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
