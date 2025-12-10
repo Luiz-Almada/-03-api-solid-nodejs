9f03912d5af6cef4bbff9e06d246a36e0e4eebb9
Luiz Almada <luizalmada@yahoo.com>
2025-12-10 18:56:32 -0300

    feat: create controllees and tests e2e of users, gyms and check-ins

---
+++ src/http/controllers/gyms/routes.ts
@@ -0,0 +1,15 @@
+import type { FastifyInstance } from 'fastify'
+
+import { VerifyJWT } from '@/http/middlewares/verify-jwt'
+import { create } from './create'
+import { search } from './search'
+import { nearby } from './nearby'
+
+export async function gymsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', VerifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
+}
